5ad6c8910234f0c10265d6c955c6a394c8e332b9
Richard Koranteng <richard@rkkoranteng.com>
2025-06-17 17:05:14 -0400

    add create EMP

---
+++ scripts/create_table_emp.sql
@@ -0,0 +1,11 @@
+-- author: Richard Koranteng
+-- description: script to create EMP table
+--
+-- 2025.06.17 : create emp statement
+
+CREATE TABLE USERS.EMP (
+    EMP_ID VARCHAR2(9) NOT NULL,
+    EMP_NAME VARCHAR2(100) NOT NULL,
+    EMP_DOB DATE NOT NULL,
+    EMP_GENDER VARCHAR(1) NOT NULL
+);
